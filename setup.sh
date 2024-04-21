93208648f87bf5f9d6cf177c700fe7ba6e1b3c3a
23ds1000103 <167696994+23ds1000103@users.noreply.github.com>
2024-04-21 23:48:09 +0530

    Update setup.sh for theme

--- setup.sh
+++ setup.sh
@@ -11,3 +11,12 @@ headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
+
+echo "\
+[theme]\n\
+backgroundColor='#781f1980'\n\
+secondaryBackgroundColor='#d5a54b63'\n\
+" > ~/.streamlit/config.toml
+
+
+
